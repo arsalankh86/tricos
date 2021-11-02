3a4c3f88731649b55bae26843734f180fe2a05c9
Arsalan <softwares.coders@gmail.com>
2021-11-01 17:07:36 -0700

    Add Typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const check = () =>
+  {
+    alert('Test');
+  }
+
+  return (
+    <div className="App">
+     <button > Typescript </button> 
+    </div>
+  );
+}
+
+export default App;
